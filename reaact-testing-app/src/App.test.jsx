7f7935135610374ab2a761512c4010578cceb023
Bablu Mehta <babloomehta895012@gmail.com>
2024-06-29 21:51:05 +0530

    'logRoles' method for debugging roles

--- reaact-testing-app/src/App.test.jsx
+++ reaact-testing-app/src/App.test.jsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
+// import { logRoles } from "@testing-library/dom";
 import App from "./App";
 
 test("button starts with correct color and text.", () => {
-  render(<App />);
+  const {container} = render(<App />);
+
+  // logRoles(container); it gives present the roles in the dom 
 
   const buttonElement = screen.getByRole("button", { name: /blue/i });
   expect(buttonElement).toHaveClass("red");
