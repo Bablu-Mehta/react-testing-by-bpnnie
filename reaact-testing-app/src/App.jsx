088ae1635bdbe0547cb594fd1bde56bee5814aec
Bablu Mehta <babloomehta895012@gmail.com>
2024-06-29 22:10:40 +0530

    Manual Acceptance Testing

--- reaact-testing-app/src/App.jsx
+++ reaact-testing-app/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button className="red">Change to blue</button>
+      <button
+        className={buttonColor}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
