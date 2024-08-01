532971a726f7c6286fb469d66a49b8c07bd13dc0
Bablu Mehta <babloomehta895012@gmail.com>
2024-08-01 08:37:27 +0530

    OPTIONAL React Code: Options and ScoopOptions Components

--- sundae-starter/src/pages/entery/Options.jsx
+++ sundae-starter/src/pages/entery/Options.jsx
@@ -1,3 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
 export default function Options({ optionType }) {
-  return <div />;
+  const [items, setItems] = useState([]);
+  //optionScoop is 'scoop' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        //TODO: handle error response
+      });
+  }, [optionType]);
+
+  //TODO: replace 'null' with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item?.name}
+      name={item?.name}
+      imagePath={item?.imagePath}
+    />
+  ));
+
+  return (
+    <Row>
+      <optionItems />
+    </Row>
+  );
 }
