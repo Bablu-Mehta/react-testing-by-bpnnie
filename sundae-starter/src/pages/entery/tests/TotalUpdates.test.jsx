d7e554ca1cdbbc75893170ccdacd8e09295eefdf
Bablu Mehta <babloomehta895012@gmail.com>
2024-08-22 08:21:17 +0530

    Code Quiz! Toppings Subtotal in progress

--- sundae-starter/src/pages/entery/tests/TotalUpdates.test.jsx
+++ sundae-starter/src/pages/entery/tests/TotalUpdates.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import { expect } from "vitest";
 
 test("should update scoop subtotal when scoops changes", async () => {
   const user = userEvent.setup();
@@ -29,4 +30,25 @@ test("should update scoop subtotal when scoops changes", async () => {
   await user.type(chocolateInput, "2");
 
   expect(scoopsSubTotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change.", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  //make sure total starts out at $0.00
+  const toppingsTotal = screen.getByText("Topping total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  //add cherries  and check total
+  const cherriesCheckBox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckBox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  //add hot fudge and check subTotal
+  const hotFudgeCheckbox = screen.getByRole("checkbox", { name: "Hot fudge" });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent('3.00');
 });
